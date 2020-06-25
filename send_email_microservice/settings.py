b6eb375a3313bdd3b917a80ca13b1c304fdba449
www-data User <www-data@JollyServer>
2020-06-25 12:06:33 +0200

    docker updates

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # #SMTP SERVER SETTINGS SENDGRID
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
