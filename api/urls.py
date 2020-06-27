ff57fcc95c7b4d27666e96e684f7662d94e92803
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2020-06-27 12:16:52 +0100

    Predefined password reset email

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from . import views, include
 
 urlpatterns = [
     path('sendmail', views.SendMail.as_view()),
     path('sendmailwithtemplate', views.SendMailWithTemplate.as_view()),
-    path('register', views.UserCreate.as_view(), name='account-create')
+    path('register', views.UserCreate.as_view(), name='account-create'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
