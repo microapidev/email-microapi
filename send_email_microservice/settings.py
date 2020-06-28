9983b32ddaa25651ec6d5d9895addd21bf16cc67
Oluwafemi <phemmylintry@gmail.com>
2020-06-28 19:41:29 +0100

    init commit

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -148,6 +149,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
 
 
 #SMTP SERVER SETTINGS SENDGRID
