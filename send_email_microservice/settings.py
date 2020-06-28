6c14b23170e17f757e9ccb98ec08aa8c784eeca0
Oluwafemi <phemmylintry@gmail.com>
2020-06-28 21:25:35 +0100

    fix

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
