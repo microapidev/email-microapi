c94e2904e5a8379509f3e9ec898f19a6c555af1b
Oluwafemi <phemmylintry@gmail.com>
2020-06-29 15:12:02 +0100

    minor fix

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mail_admin',
 
     #third party apps
     'rest_framework',
