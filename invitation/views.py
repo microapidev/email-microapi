80669b1ae504f949fff781c592154eb07cebb674
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2020-07-02 15:46:54 +0100

    Password reset with template

--- invitation/views.py
+++ invitation/views.py
@@ -60,3 +60,24 @@ class SendInvitationLink(APIView):
                     'status': 'failure',
                     'data': {'message': 'Something went wrong'}
                 }, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+
+class PasswordResetForm(forms.Form):
+     email = forms.EmailField(label=_("Email"), max_length=254)
+
+     def send_mail(self, subject_template_name, email_template_name,
+              context, from_email, to_email, html_email_template_name=None):
+              """
+               Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+              """
+             subject = loader.render_to_string(subject_template_name, context)
+             # Email subject *must not* contain newlines
+             subject = ''.join(subject.splitlines())
+             body = loader.render_to_string(email_template_name, context)
+
+             email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+             if html_email_template_name is not None:
+                 html_email = loader.render_to_string(html_email_template_name, context)
+                 email_message.attach_alternative(html_email, 'text/html')
+
+             email_message.send()```
--- password_reset/urls.py
+++ password_reset/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 from django.views.decorators.csrf import csrf_exempt
 
+from django.conf.urls import url, include
+from password_reset.views import reset_password_request_token, reset_password_confirm, reset_password_validate_token
+
 urlpatterns = [
     #path('sendmail/', views.SendMail.as_view()),
+    url(r'^validate_token/', reset_password_validate_token, name="reset-password-validate"),
+    url(r'^confirm/', reset_password_confirm, name="reset-password-confirm"),
+    url(r'^', reset_password_request_token, name="reset-password-request"),
 ]
+
