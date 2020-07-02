181f4907d9d05b9da44b4de857302628fe5c25b2
Oluwafemi <phemmylintry@gmail.com>
2020-07-02 22:30:20 +0100

    update

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0#iz6gmldf!6jvht^n6ub#^%m=zd85e&xw7-q3l5enrkwn%@p3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'email-microdev.herokuapp.com']
 
