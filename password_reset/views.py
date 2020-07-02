07fe7ad38075a9e97836753be81ac56027e6f12b
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2020-07-02 15:59:05 +0100

    Added custom password reset form class in its right place, in the password reset folder

--- password_reset/views.py
+++ password_reset/views.py
@@ -196,4 +196,22 @@ reset_password_validate_token = ResetPasswordValidateToken.as_view()
 reset_password_confirm = ResetPasswordConfirm.as_view()
 reset_password_request_token = ResetPasswordRequestToken.as_view()
 
+class PasswordResetForm(forms.Form):
+     email = forms.EmailField(label=_("Email"), max_length=254)
 
+     def send_mail(self, subject_template_name, email_template_name,
+              context, from_email, to_email, html_email_template_name=None):
+              """
+               Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+              """
+             subject = loader.render_to_string(subject_template_name, context)
+             # Email subject *must not* contain newlines
+             subject = ''.join(subject.splitlines())
+             body = loader.render_to_string(email_template_name, context)
+
+             email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+             if html_email_template_name is not None:
+                 html_email = loader.render_to_string(html_email_template_name, context)
+                 email_message.attach_alternative(html_email, 'text/html')
+
+             email_message.send()```
