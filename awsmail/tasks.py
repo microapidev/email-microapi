6d0be68334854cd93a52c3e9e8962918695279bd
Oluwafemi <phemmylintry@gmail.com>
2020-07-09 10:26:27 +0100

    init celery and rabbitmq

--- awsmail/tasks.py
+++ awsmail/tasks.py
@@ -1,12 +1,11 @@
 from celery import shared_task
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
-from api.serializers import MailSerializer
 from django.core.mail import send_mail
 from time import sleep
 
 
 @shared_task()
 def send_aws(subject, body, sender, recipient):
-    return send_mail(subject, body, sender, [recipient], fail_silently=False)
+    send_mail(subject, body, sender, [recipient], fail_silently=False)
+    sleep(5)
+    return send_mail()
