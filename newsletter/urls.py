c8e054ee9bc40f693317196f3fbf6b5f5c7935c2
Nextafari <chinedue16@gmail.com>
2020-07-10 02:39:46 +0100

    modified

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('newsletter_all/', views.DisplayAll.as_view(), name='DisplayAll'),
     path('create_newsletter/', views.SendEmail.as_view(), name='SendEmail'),
-    path('custom_newsletter/', views.SendCustomMail.as_view(), 'SendCustomMail'),
+    path('custom_newsletter/', views.SendCustomMail.as_view(), name='SendCustomMail'),
 ]
