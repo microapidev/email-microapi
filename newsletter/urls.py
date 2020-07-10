99562504b11095b6304231178489686f118d5af8
Nextafari <chinedue16@gmail.com>
2020-07-10 02:38:51 +0100

    modified

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('newsletter_all/', views.DisplayAll.as_view()),
-    path('create_newsletter/', views.SendEmail.as_view()),
-    path('custom_newsletter/', views.SendCustomMail.as_view()),
+    path('newsletter_all/', views.DisplayAll.as_view(), name='DisplayAll'),
+    path('create_newsletter/', views.SendEmail.as_view(), name='SendEmail'),
+    path('custom_newsletter/', views.SendCustomMail.as_view(), 'SendCustomMail'),
 ]
