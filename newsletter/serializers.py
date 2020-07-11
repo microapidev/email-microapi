9e70598d19015c768c011421d5b29abde641b2a5
Nextafari <chinedue16@gmail.com>
2020-07-11 22:34:14 +0100

    commented codes

--- newsletter/serializers.py
+++ newsletter/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Newsletter
 
 class NewsletterSerializers(serializers.ModelSerializer):
+    """Serializing fields/objects for creating and sending emails on the go"""
     class Meta:
         model = Newsletter
         fields = ('subject', 'body', 'from_email', 'to_email')
 
 
 class CustomSerializers(serializers.ModelSerializer):
+    """Serializing fields/objects for sending predefined newsletters"""
     class Meta:
         model = Newsletter
         fields = ('subject', 'from_email', 'to_email')
