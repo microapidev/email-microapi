d3dd1a955879ece943219d11e5047676f47e6924
phemmylintry <phemmylintry@gmail.com>
2020-07-12 11:31:54 +0100

    fix

--- send_email_microservice/settings.py
+++ send_email_microservice/settings.py
@@ -62,8 +62,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
