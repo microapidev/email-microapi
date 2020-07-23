df676102bcb09e10faa07f0b9dd5b01318599569
Nextafari <chinedue16@gmail.com>
2020-07-23 18:22:34 +0100

    Feat

--- newsletter_with_frontend/admin.py
+++ newsletter_with_frontend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "content",
+        "created",
+    ]
+
+
+admin.site.register(UserProfile, AdminUserProfile)
--- newsletter_with_frontend/views.py
+++ newsletter_with_frontend/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .models import UserProfile
+from rest_framework.decorators import api_view
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from .serializer import UserProfileSerializers, EditSerializer
 
-# Create your views here.
+
+
+@swagger_auto_schema(
+    request_body=UserProfileSerializers,
+    operation_description="Gets User Profile and Contents.",
+    tags=['Gets User Profile and Contents']
+	
+@api_view(['GET'])
+def profile_list(request):
+
+    if request.method == 'GET':
+        users = UserProfile.objects.all()
+        serializer = UserProfileSerializers(users, many=True)
+        return Response(serializer.data)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_user_profile(request, pk):
+    try:
+        user = UserProfile.objects.get(pk=pk)
+    except UserProfile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserProfileSerializers(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = EditSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
